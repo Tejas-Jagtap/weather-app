6f8766b958e9baeca0aea5b01a88f4ae691ce553
Tejas-Jagtap <tejasannasojagtap@gmail.com>
2022-04-30 14:07:11 +0530

    forcast change

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const forecast = (latitude, longitude, callback) => {
 
     const url = "http://api.weatherstack.com/current?access_key=b90e1b784b8276a92e2ebaa35361040b&query=" + latitude + "," + longitude + "&units=m"
 
-    request({ url, json: true }, (error, {body}={}) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback("We are unable to connect weather services", undefined)
         }
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
             callback("Unable to find location", undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + ", Its currently " + body.current.temperature + " degrees out, but it feels like " + body.current.feelslike + " degrees.")
+            callback(undefined, body.current.weather_descriptions[0] + ", Its currently " + body.current.temperature + " degrees out, but it feels like " + body.current.feelslike + " degrees. The humidity is " + body.current.humidity + '.')
         }
 
     })
